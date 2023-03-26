399b07f0d041b3affc197030148060c76aded603
wtvamp <warrenjthompson@gmail.com>
2023-03-25 20:14:30 -0700

    Initial Commit

---
+++ AutismAssistMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AutismAssistMobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ AutismAssistMobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AutismAssistMobile;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
